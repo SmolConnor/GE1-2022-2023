dc2e86f79b37aeb0fd8f485d1d1ab638507c47a4
SmolConnor <77941492+SmolConnor@users.noreply.github.com>
2022-10-13 23:22:17 +0100

    basic values and rotation attempts

--- GE1 Examples 2022/Assets/fish.cs
+++ GE1 Examples 2022/Assets/fish.cs
@@ -7,15 +7,22 @@ public class fish : MonoBehaviour
 
     public Transform head;
     public Transform tail;
+    public float Theta;
+    [Range(0.1f, 3.0f)]
+    public float frequincy;
+    public float headAmplitude;
+    public float tailAmplitude;
     // Start is called before the first frame update
     void Start()
     {
-        
+        frequincy = 0.6f;
+        headAmplitude = 10f;
+        tailAmplitude = 10f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        head.localRotation = Quaternion.AngleAxis(headAmplitude, Vector3.forward);
     }
 }
